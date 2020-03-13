36af2fcb46456f8be6a9eb58609679d9389b10bf
ilslv <ilya.solovyiov@gmail.com>
2020-03-13 16:31:40 +0300

    Lab5 done

---
+++ task5/src/twitter/Test2.java
@@ -0,0 +1,13 @@
+package twitter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Test2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("/page");
+    }
+}
